bf2923ec022bdf7cf8abb1d5f545c45c8345032e
Nathan Stene <nathanstene@gmail.com>
2024-10-29 11:30:42 -0400

    feat: add presentation page

---
+++ src/app/presentation/page.tsx
@@ -0,0 +1,9 @@
+// `app/dashboard/page/tsx is the UI for the `/dashboard` url
+
+export default function Page() {
+    return ( 
+        <h1>
+            Hello, this is my presentation page!
+        </h1>
+    )
+}
