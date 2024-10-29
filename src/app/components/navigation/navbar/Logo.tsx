6412ff4b12dde28ac3143fea9b5bbc150bc870de
Nathan Stene <nathanstene@gmail.com>
2024-10-29 13:00:50 -0400

    feat: working navigation bar with logo

---
+++ src/app/components/navigation/navbar/Logo.tsx
@@ -0,0 +1,59 @@
+"use client"; // enable usage of useState hook
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Button from "./Button";
+
+const Logo = () => {
+  //update the size of the logo when the size of the screen changes
+  const [width, setWidth] = useState(0);
+
+  const updateWidth = () => {
+    const newWidth = window.innerWidth;
+    setWidth(newWidth);
+  };
+
+  // updateWidth function is placed inside the useEffect hook because it references the 'window' object, which is only available on the client side.
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    updateWidth();
+  }, []);
+
+  // change between the logo and the button when the user scrolls
+  const [showButton, setShowButton] = useState(false);
+
+  const changeNavButton = () => {
+    if (window.scrollY >= 400 && window.innerWidth < 768) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavButton);
+  }, []);
+
+  return (
+    <>
+      <Link href="/" style={{ display: showButton ? "none" : "block" }}>
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          width={width < 1024 ? "75" : "250"}
+          height={width < 1024 ? "75" : "74"}
+          className="relative"
+        />
+      </Link>
+      <div
+        style={{
+          display: showButton ? "block" : "none",
+        }}
+      >
+        <Button />
+      </div>
+    </>
+  );
+};
+
+export default Logo;
--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -1,21 +1,16 @@
 import React from "react";
 import Link from "next/link";
-//import Logo from "./Logo";
-//import Button from "./Button";
+import Logo from "./Logo";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-30 bg-emerald-800 sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
-            {/* <Logo /> */}
+            <Logo />
             <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/">
-                  <p>Home</p>
-                </Link>
-              </li>
               <li>
                 <Link href="/Journal">
                   <p>Journal</p>
@@ -37,7 +32,7 @@ const Navbar = () => {
                 </Link>
               </li>
             </ul>
-            {/* <Button /> */}
+            <Button />
           </div>
         </div>
       </div>
