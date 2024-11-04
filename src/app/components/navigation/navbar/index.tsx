acc3979a58b2156652cf855e894ded397ce45406
Nathan Stene <nathanstene@gmail.com>
2024-11-04 14:03:59 -0500

    feat: make dropdown menu for 2d artwork

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useRef } from "react";
 import Link from "next/link";
 //import AuthButton from "./AuthButton";
 
@@ -27,6 +27,24 @@ function AuthButton1() {
 //export default AuthButton;
 
 const Navbar = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  const handleMouseEnter = () => {
+    // Clear any existing timeout when mouse enters
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+    setDropdownOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    // Set a timeout to close the dropdown after x milliseconds
+    timeoutRef.current = setTimeout(() => {
+      setDropdownOpen(false);
+    }, 250); // Time for the menu to stay open, in milliseconds
+  };
+
   return (
     <>
       <div className="w-full h-30 bg-emerald-800 sticky top-0">
@@ -48,11 +66,35 @@ const Navbar = () => {
                   <p>3D artwork</p>
                 </Link>
               </li>
-              <li>
+              <li
+                onMouseEnter={handleMouseEnter} 
+                onMouseLeave={handleMouseLeave} 
+                className="relative" // This is to position the dropdown correctly
+              >
                 <Link href="/classic-routes/TwoDPage">
                   <p>2D artwork</p>
                 </Link>
-              </li>
+
+              {isDropdownOpen && (
+                <ul className="absolute left-0 mt-2 w-48 bg-white text-black shadow-lg">
+                  <li>
+                    <Link href="/classic-routes/TwoDPage1">
+                      <p className="p-2 hover:bg-gray-200">2D Art 1</p>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/classic-routes/TwoDPage2">
+                      <p className="p-2 hover:bg-gray-200">2D Art 2</p>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/classic-routes/TwoDPage3">
+                      <p className="p-2 hover:bg-gray-200">2D Art 3</p>
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
               <li>
                 <Link href="/classic-routes/trip-reports">
                   <p>Trip reports</p>
