3a48364b0b112e287955114f5b8b9a2eb9aa02c8
Nathan Stene <nathanstene@gmail.com>
2024-11-14 14:54:05 -0500

    fix: fix typescript error

--- src/app/components/navigation/MobileNavbar/index.tsx
+++ src/app/components/navigation/MobileNavbar/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import AuthButton from "../AuthButton/AuthButton";
