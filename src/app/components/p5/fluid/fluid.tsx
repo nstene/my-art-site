58d3aec0dd73069510e0edcca83cf21b1abbe82e
Nathan Stene <nathanstene@gmail.com>
2024-11-21 14:44:37 -0500

    feat: issue in query

--- src/app/components/p5/fluid/fluid.tsx
+++ src/app/components/p5/fluid/fluid.tsx
@@ -9,19 +9,20 @@ import p5 from 'p5';
 export const MySketch = () => (p: p5) => {
     const width = p.windowWidth;
     const height = p.windowHeight;
-    const nParticles = 500;
-    const particleRadius = 10;
+    const nParticles = 10;
+    const particleRadius = 100;
     let particles: Particle[] = [];
     const initialVelocity = 0;
     const g = 0 / 10;
     const frameRate = 40;
     let t: number;
-    const collisionDamp = 0.95;
-    const influenceRadius = particleRadius * 20;
+    const collisionDamp = 0;
+    const influenceRadius = particleRadius * 2;
     let densities: number[] = [];
     const mass = 1;
     const targetDensity = 0.00001;
-    const pressureMultiplier = 1000;
+    const pressureMultiplier = 10e3;
+    const maxDist = 2 * particleRadius; // see what it changes?
 
     class Position {
         x: number;
@@ -93,48 +94,6 @@ export const MySketch = () => (p: p5) => {
         return - 12 * (influenceRadius - distance) / p.PI / Math.pow(influenceRadius, 4)
     }
 
-    const ComputeDensity = (particles: Particle[], samplePoint: Position): number => {
-
-        let density = 0;
-
-        for (let particle of particles) {
-            const distance = Math.hypot((particle.position.x - samplePoint.x), (particle.position.y - samplePoint.y));
-            const influence = SmoothingKernel(influenceRadius, distance);
-            density += mass * influence;
-        }
-        return density;
-    };
-
-    const UpdateDensities = (particles: Particle[]): number[] => {
-
-        densities = [];
-        for (let particle of particles) {
-            //const particlesToUse = ForeachPointWithinRadius(particles, particle, spatialLookup, startIndices);
-            const density = ComputeDensity(particles, particle.position)
-            densities.push(density);
-        };
-
-        return densities
-    };
-
-    const ComputePressureForce = (particles: Particle[], particleIndex: number, densities: number[]): PressureForce => {
-        let pressureForce = [0, 0];
-
-        for (let i = 0; i < particles.length; i++) {
-            const distance = Math.hypot((particles[i].position.x - particles[particleIndex].position.x), (particles[i].position.y - particles[particleIndex].position.y));
-            if ( distance <= 10e-8 ){
-                continue;
-            }
-            const slope = SmoothingKernelDerivative(influenceRadius, distance);
-            const direction = [(particles[i].position.x - particles[particleIndex].position.x) / distance, (particles[i].position.y - particles[particleIndex].position.y) / distance];
-            const sharedPressure = ComputeSharedPressure(densities[particleIndex], densities[i]);
-            pressureForce[0] += sharedPressure * direction[0] * slope * mass / densities[particleIndex];
-            pressureForce[1] += sharedPressure * direction[1] * slope * mass / densities[particleIndex];
-        }
-
-        return new PressureForce(pressureForce[0], pressureForce[1])
-    }
-
 
     const ConvertDensityToPressure = (density: number): number => {
         const densityDiff = density - targetDensity;
@@ -149,89 +108,106 @@ export const MySketch = () => (p: p5) => {
     };
 
 
-    // Convert a position to the coordinate of the cell it is in
-    const PositionToCellCoord = (point: Position, radius: number): Position => {
-        const cellX = Math.round(point.x / radius);
-        const cellY = Math.round(point.y / radius);
-        return new Position(cellX, cellY)
-    };
+    class Hash {
+        spacing: number; // will probably be twice the influenceRadius
+        tableSize: number;
+        cellStart: number[];
+        cellEntries: number[];
+        queryIds: number[];
+        querySize: number;
 
-
-    // Convert a cell coordinate into a single number
-    // Hash collisions (having different cells mapping to the same value) are unavoidable.
-    const HashCell = (cellPosition: Position): number => {
-        const a = cellPosition.x * 15823;
-        const b = cellPosition.y * 9737333;
-        return a + b
-    };
-
-
-    // Wrap the hash value around the length of the array (so it can be used as an index)
-    const GetKeyFromHash = (hash: number, spatialLookup: Entry[]): number => {
-        return hash % spatialLookup.length
-    };
-
-
-    const UpdateSpatialLookup = (particles: Particle[], influenceRadius: number): [Entry[], number[]] => {
-        let startIndices = [];
-        let spatialLookup: Entry[] = [];
-        // Create spatial lookup
-        for (let i = 0; i < particles.length; i++) {
-            const cellCoordinates = PositionToCellCoord(particles[i].position, influenceRadius);
-            const cellKey = GetKeyFromHash(HashCell(cellCoordinates), spatialLookup);
-            spatialLookup[i] = new Entry(i, cellKey);
-            startIndices[i] = Number.MAX_SAFE_INTEGER; // Initialize start indices
+        constructor(spacing: number, maxNumObjects: number) {
+            this.spacing = spacing;
+            this.tableSize = 2 * maxNumObjects;
+            this.cellStart = new Array(this.tableSize + 1);
+            this.cellEntries = new Array(maxNumObjects);
+            this.queryIds = new Array(maxNumObjects);
+            this.querySize = 0;
         }
 
-        // Sort by cell key
-        spatialLookup.sort();
 
-        // Calculate start indices of each unique cell key in the spatial lookup
-        for (let i = 0; i < particles.length; i++) {
-            const key = spatialLookup[i].cellKey
-            const keyPrev = i == 0 ? Number.MAX_SAFE_INTEGER : spatialLookup[i - 1].cellKey
-            if (key != keyPrev) {
-                startIndices[key] = i;
+        // Converts particle coordinates into cell coordinates
+        positionToCellCoord(point: Position): Position {
+            const cellX = Math.round(point.x / this.spacing);
+            const cellY = Math.round(point.y / this.spacing);
+            return new Position(cellX, cellY)
+        };
+
+
+        // Convert a cell coordinate into a single number
+        // Hash collisions (having different cells mapping to the same value) are unavoidable.
+        hashCoords(cellPosition: Position): number {
+            const prime1 = 73856093; // Large prime numbers for hashing
+            const prime2 = 19349663;
+            const h = (cellPosition.x * prime1 + cellPosition.y * prime2) % this.tableSize;
+            return h < 0 ? h + this.tableSize : h; // Ensure positive hash value
+        }
+
+
+        // Takes the position of a particle and returns the hash = index in the flat grid array
+        hashPos(position: Position): number {
+            return this.hashCoords(this.positionToCellCoord(position));
+        }
+
+
+        // Create the grid hasing
+        create(particles: Particle[]) {
+            let numObjects = Math.min(particles.length, this.cellEntries.length);
+
+            // determine cell sizes
+
+            this.cellStart.fill(0);
+            this.cellEntries.fill(0);
+
+            for (let i = 0; i < numObjects; i++) {
+                let h = this.hashPos(particles[i].position);
+                this.cellStart[h]++;
+            }
+
+            // determine cells starts
+
+            let start = 0;
+            for (let i = 0; i < this.tableSize; i++) {
+                start += this.cellStart[i];
+                this.cellStart[i] = start;
+            }
+            this.cellStart[this.tableSize] = start; // guard
+
+            // fill in objects ids
+
+            for (let i = 0; i < numObjects; i++) {
+                let h = this.hashPos(particles[i].position);
+                this.cellStart[h]--;
+                this.cellEntries[this.cellStart[h]] = i;
             }
         }
-        return [spatialLookup, startIndices]
-    }
 
 
-    const ForeachPointWithinRadius = (particles: Particle[], particle: Particle, spatialLookup: Entry[], startIndices: number[]): Particle[] => {
-        // Find which cell the sample point is in (make it the center of our 3x3 block)
-        const centreCoord = PositionToCellCoord(particle.position, influenceRadius);
-        const indicesToUse = [];
-        const particlesToUse: Particle[] = [];
+        // Query the particles around the one
+        query(particles: Particle[], nr: number, maxDist: number) {
+            const minPositionCoordinates = new Position(particles[nr].position.x - maxDist, particles[nr].position.y - maxDist);
+            const minCellCoordinates = this.positionToCellCoord(minPositionCoordinates);
 
-        // Loop over all cells of the 3x3 block around the center cell
-        for (let offsetX = -1; offsetX <= 1; offsetX++) {
-            for (let offsetY = -1; offsetY <= 1; offsetY++) {
-                const cellCoord = new Position(centreCoord.x + offsetX, centreCoord.y + offsetY);
-                const key = GetKeyFromHash(HashCell(cellCoord), spatialLookup);
-                const cellStartIndex = startIndices[key];
+            const maxPositionCoordinates = new Position(particles[nr].position.x + maxDist, particles[nr].position.y + maxDist);
+            const maxCellCoordinates = this.positionToCellCoord(maxPositionCoordinates);
 
-                for (let i = cellStartIndex; i < spatialLookup.length; i++) {
+            this.querySize = 0;
+            this.queryIds = [];
 
-                    // Exit loop if we're no longer looking at the correct cell
-                    if (spatialLookup[i].cellKey != key) break;
+            for (let xi = minCellCoordinates.x; xi <= maxCellCoordinates.x; xi++) {
+                for (let yi = minCellCoordinates.y; yi <= maxCellCoordinates.y; yi++) {
+                    const h = this.hashCoords(new Position(xi, yi));
+                    const start = this.cellStart[h];
+                    const end = this.cellStart[h + 1];
 
-                    const particleIndex = spatialLookup[i].particleIndex;
-
-                    const distance = Math.hypot((particles[particleIndex].position.x - particle.position.x), (particles[particleIndex].position.y - particle.position.y));
-
-                    // Double check if the point is within the radius
-                    if (distance <= influenceRadius) {
-                        // Add it to the particles to be used
-                        indicesToUse.push(particleIndex);
-                        particlesToUse.push(particles[particleIndex]);
+                    for (let i = start; i < end; i++) {
+                        this.queryIds[this.querySize] = this.cellEntries[i];
+                        this.querySize++;
                     }
                 }
             }
+
         }
-        //const density = ComputeDensity(particlesToUse, particle.position);
-        //const pressureForce = ComputePressureForce(particlesToUse, particle, density);
-        return particlesToUse
     }
 
 
@@ -257,57 +233,151 @@ export const MySketch = () => (p: p5) => {
 
         t = p.frameCount / frameRate;
 
-        // Update spatial look up
-        //const [spatialLookup, startIndices] = UpdateSpatialLookup(particles, influenceRadius);
-        const densities = UpdateDensities(particles);
+        // Hash that shit
+        const hash = new Hash(2 * influenceRadius, nParticles)
+        const hash2 = new Hash(2 * influenceRadius, nParticles)
+        hash.create(particles)
+        hash2.create(particles)
 
-        let sum = 0;
-        for (let i in particles) {
-            sum += densities[i];
-        }
-        const mean_density = sum / densities.length
-
-        p.push();
-        p.noStroke();
-        p.text(`${mean_density}`, 0, p.windowHeight / 2);
-        p.pop();
 
         // Update the particle positions where they're supposed to be after t, with correct pressure forces
         for (let particleIndex = 0; particleIndex < particles.length; particleIndex++) {
 
             const particle = particles[particleIndex];
 
-           // const particlesToUse = ForeachPointWithinRadius(particles, particle, spatialLookup, startIndices)
+            hash.query(particles, particleIndex, maxDist);
 
-            const pressureForce = ComputePressureForce(particles, particleIndex, densities)
+            // Compute density
+            let density = 0;
+            for (let i = 0; i < hash.querySize; i++) {
+                const otherParticle = particles[hash.queryIds[i]];
 
-            let acceleration_y = g
+                let distance = 0;
+                try {
+                    distance = Math.hypot((otherParticle.position.x - particle.position.x), (otherParticle.position.y - particle.position.y));
+                } catch {
+                    continue;
+                }
+                const influence = SmoothingKernel(influenceRadius, distance);
+                density += mass * influence;
+            }
+
+            // Compute pressure force
+            let pressureForce = [0, 0];
+            for (let j = 0; j < hash.querySize; j++) {
+                const otherParticle = particles[hash.queryIds[j]];
+
+                let distance = 0;
+                try {
+                    distance = Math.hypot((otherParticle.position.x - particle.position.x), (otherParticle.position.y - particle.position.y));
+                } catch {
+                    continue;
+                }
+
+                if (distance <= 10e-8) {
+                    // we're probably comparing a particle to itself
+                    continue;
+                }
+
+                // Compute density for other particle neighbourhood
+                hash2.query(particles, hash.queryIds[j], maxDist);
+                let densityOtherParticle = 0;
+                for (let k = 0; k < hash2.querySize; k++) {
+                    const thirdParticle = particles[hash2.queryIds[k]];
+
+                    let newDistance = 0;
+                    try {
+                        newDistance = Math.hypot((thirdParticle.position.x - otherParticle.position.x), (thirdParticle.position.y - otherParticle.position.y));
+                    } catch {
+                        continue;
+                    }
+                    const influence = SmoothingKernel(influenceRadius, newDistance);
+                    densityOtherParticle += mass * influence;
+                }
+
+                // Shared pressure and pressure force
+                const slope = SmoothingKernelDerivative(influenceRadius, distance);
+                const direction = [(otherParticle.position.x - particle.position.x) / distance, (otherParticle.position.y - particle.position.y) / distance];
+                const sharedPressure = ComputeSharedPressure(density, densityOtherParticle);
+                if (density <= 10e-8) {
+                    continue;
+                }
+                pressureForce[0] += sharedPressure * direction[0] * slope * mass / density;
+                pressureForce[1] += sharedPressure * direction[1] * slope * mass / density;
+            }
+
+            let acceleration_y = g;
             let acceleration_x = 0;
-            if (densities[particleIndex] !== 0) {
-                acceleration_y += pressureForce.y / densities[particleIndex];
-                acceleration_x += pressureForce.x / densities[particleIndex];
-            } 
+
+            if (density >= 10e-8) {
+                acceleration_y += pressureForce[1] / density;
+                acceleration_x += pressureForce[0] / density;
+            }
+
             particle.velocity.y = acceleration_y * t;
             particle.velocity.x = acceleration_x * t;
 
             particle.position.y += particle.velocity.y * t;
             particle.position.x += particle.velocity.x * t;
 
-            // Resolve collisions
+            // Resolve world collisions
             if ((particle.position.y + particleRadius) >= height || (particle.position.y - particleRadius) <= 0) {
-                particle.position.y = Math.min(height - 1, Math.max(0, particle.position.y));
-                particle.velocity.y *= -1 * collisionDamp;
+                particle.position.y = Math.min(height - particleRadius, Math.max(particleRadius, particle.position.y));
+                particle.velocity.y *= -1 * ( 1 - collisionDamp );
             }
 
             if ((particle.position.x + particleRadius) >= width || (particle.position.x - particleRadius) <= 0) {
-                particle.position.x = Math.min(width - 1, Math.max(0, particle.position.x));
-                particle.velocity.x *= -1 * collisionDamp;
+                particle.position.x = Math.min(width - particleRadius, Math.max(particleRadius, particle.position.x));
+                particle.velocity.x *= -1 * ( 1 - collisionDamp );
+            }
+
+            // Check for overlaps
+            hash.query(particles, particleIndex, 2.0 * particleRadius);
+            for (let i = 0; i < hash.querySize; i++) {
+                const otherParticle = particles[hash.queryIds[i]];
+
+                let distance = 0;
+                try {
+                    distance = Math.hypot((otherParticle.position.x - particle.position.x), (otherParticle.position.y - particle.position.y));
+                } catch {
+                    continue;
+                }
+
+                if ( distance <= 10e-8 ) { 
+                    continue;
+                }
+
+                const minDistance = 2*particleRadius;
+                
+                if ( distance <= minDistance ) {
+                    // Separate the balls
+                    const correction = (minDistance - distance)/2;
+                    const normalDirection = [otherParticle.position.x - particle.position.x, otherParticle.position.y - particle.position.y];
+                    particle.position.x += normalDirection[0] * correction;
+                    particle.position.y += normalDirection[1] * correction;
+                    otherParticle.position.x -= normalDirection[0] * correction;
+                    otherParticle.position.y -= normalDirection[1] * correction;
+
+                    // Reflect velocities along normal
+                    const dotProductParticleNormal = particle.position.x * normalDirection[0] + particle.position.y * normalDirection[1];
+                    const dotProductOtherParticleNormal = otherParticle.position.x * normalDirection[0] + otherParticle.position.y * normalDirection[1];
+
+                    particle.velocity.x += normalDirection[0] * ( dotProductParticleNormal - dotProductOtherParticleNormal);
+                    particle.velocity.y += normalDirection[1] * ( dotProductParticleNormal - dotProductOtherParticleNormal);
+                    otherParticle.velocity.x -= normalDirection[0] * ( dotProductParticleNormal - dotProductOtherParticleNormal);
+                    otherParticle.velocity.y -= normalDirection[1] * ( dotProductParticleNormal - dotProductOtherParticleNormal);
+                }
             }
         }
 
         // Draw the particles where they're supposed to be
         for (let particle of particles) {
-            p.circle(particle.position.x, particle.position.y, particleRadius);
+            p.circle(particle.position.x, particle.position.y, 2 * particleRadius);
+            p.push();
+            p.fill(0);
+            p.text(`${particle.position.x}`, particle.position.x, particle.position.y);
+            p.text(`${particle.position.y}`, particle.position.x, particle.position.y + 10);
+            p.pop();
         }
 
     };
